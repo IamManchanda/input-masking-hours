7b7108ffe67767cc8a6deb9d46b521dc26b711b2
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-01-30 02:24:01 +0530

    Working pass on masking of hours
    PS: Has a bug, should only allow `23:59` or `24:00`

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="grid-x grid-margin-x grid-padding-x">
+    <div class="cell medium-4">
+      <h3>Masking</h3>
+      <input
+        type="text"
+        v-model="value"
+        ref="focusableData"
+        @click="focusInputBox"
+        @focus="focusInputBox"
+        v-mask="mask"
+        @input="handleInputBox"
+      />
+      <p>{{ maskedValue }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Inputmask from 'inputmask';
+
+// Taken from https://github.com/scleriot/vue-inputmask/blob/master/src/vue-inputmask.js
+const Mask = {
+  bind: (el, binding) => {
+    Inputmask(binding.value).mask(el);
+  },
+};
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      value: '00:00',
+      mask: '99:99',
+    };
+  },
+  directives: {
+    mask: Mask,
+  },
+  computed: {
+    maskedValue() {
+      if (this.value && this.mask) return Inputmask.format(this.value, this.mask);
+      return this.value;
+    },
+  },
+  methods: {
+    focusInputBox() {
+      const input = this.$refs.focusableData;
+      input.setSelectionRange(0, input.value.length);
+    },
+    handleInputBox(event) {
+      this.$emit('input', event.target.inputmask.unmaskedvalue());
+    },
   },
 };
 </script>
